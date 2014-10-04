d175a87c893da2d692f3616d4d54f370ba421e3d
Miguel Puig <miguelpuig@gmail.com>
2014-10-04 14:32:32 +0100

    round number total

--- app/js/services.js
+++ app/js/services.js
@@ -2,12 +2,9 @@
 
 /* Services */
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
 var services = angular.module('myApp.services', []);
 
 services.value('version', '0.1');
-
 
 
 services.factory("LS", function($window, $rootScope) {
